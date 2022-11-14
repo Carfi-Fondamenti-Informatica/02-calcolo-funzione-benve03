be043a8ae5325aef84370545c8c6241e04b24db0
benve03 <117894307+benve03@users.noreply.github.com>
2022-11-14 17:32:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
+   float a=0,b=0,x=0,y=0;
+    cout <<"inserisci valori di: A,B,X e Y"<< endl;
+    cin >>a>>b>>x>>y;
+    if (x<0 & y>0){
+        cout <<(a*x)-(b*y)<< endl;
+    }else if (x>=0 & y<=0){
+        cout <<(a*(x*x))-(b*y)<< endl;
+    }else
+        cout <<(a*x)+(b*(y*y))<< endl;
    return 0;
 }
